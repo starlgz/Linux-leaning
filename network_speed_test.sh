d7d70a25370dea4feb72e2ee827bafe322bfabc8
starlgz <97614350+starlgz@users.noreply.github.com>
2023-08-08 11:45:31 +0800

    Update network_speed_test.sh

--- network_speed_test.sh
+++ network_speed_test.sh
@@ -9,18 +9,28 @@ fi
 
 # 测速函数
 function network_speed_test() {
-    echo "开始测试服务器：$1"
-    speedtest-cli --server "$2" > /dev/null
+    local server_name="$1"
+    local server_id="$2"
+
+    echo "正在测试服务器：$server_name [$server_id]..."
+    speedtest-cli --server "$server_id" > /dev/null
     echo "====================================="
 }
 
 # 执行网络测速
 function main() {
-    network_speed_test "中国上海移动" 5862
-    network_speed_test "中国北京电信" 3633
-    network_speed_test "中国广州电信" 5083
-    network_speed_test "新加坡" 18220
-    network_speed_test "美国西部" 5117
+    # 手动选择测速服务器，包含3个中国服务器和其他一些国际服务器
+    local servers=(
+        "中国上海移动" "30487"
+        "中国北京电信" "35403"
+        "中国广州电信" "5083"
+        "新加坡" "1902"
+        "美国西部" "6739"
+    )
+
+    for ((i=0; i<${#servers[@]}; i+=2)); do
+        network_speed_test "${servers[i]}" "${servers[i+1]}"
+    done
 }
 
 main

da39b251dd14e9e5fa3dbc7eb6bd711a7086203d
starlgz <97614350+starlgz@users.noreply.github.com>
2023-08-08 12:24:43 +0800

    Update network_speed_test.sh

--- network_speed_test.sh
+++ network_speed_test.sh
@@ -16,12 +16,32 @@ function check_docker_installed() {
 # 安装 Docker Engine
 function install_docker_engine() {
     echo "安装 Docker Engine..."
-    sudo apt update
-    sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-    echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo apt update
-    sudo apt install docker-ce docker-ce-cli containerd.io -y
+
+    if [ -f /etc/os-release ]; then
+        source /etc/os-release
+        case "$ID" in
+            ubuntu|debian)
+                sudo apt update
+                sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+                curl -fsSL https://download.docker.com/linux/$ID/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+                echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/$ID $VERSION_CODENAME stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+                sudo apt update
+                sudo apt install docker-ce docker-ce-cli containerd.io -y
+                ;;
+            centos|fedora)
+                sudo dnf install -y dnf-plugins-core
+                sudo dnf config-manager --add-repo https://download.docker.com/linux/$ID/docker-ce.repo
+                sudo dnf install docker-ce -y
+                ;;
+            *)
+                echo "不支持的 Linux 发行版。"
+                exit 1
+                ;;
+        esac
+    else
+        echo "无法确定 Linux 发行版。"
+        exit 1
+    fi
 }
 
 # 安装 Docker Compose
@@ -31,15 +51,31 @@ function install_docker_compose() {
     sudo chmod +x /usr/local/bin/docker-compose
 }
 
+# 卸载 Docker Engine
+function uninstall_docker_engine() {
+    echo "卸载 Docker Engine..."
+    sudo apt purge docker-ce docker-ce-cli containerd.io -y
+    sudo apt autoremove -y
+    sudo rm -rf /var/lib/docker
+}
+
+# 卸载 Docker Compose
+function uninstall_docker_compose() {
+    echo "卸载 Docker Compose..."
+    sudo rm -f /usr/local/bin/docker-compose
+}
+
 # 主函数
 function main() {
     check_docker_installed
 
-    echo "请选择安装选项："
+    echo "请选择操作选项："
     echo "1. 安装 Docker Engine"
     echo "2. 安装 Docker Compose"
     echo "3. 安装 Docker Engine 和 Docker Compose"
-    echo "4. 退出"
+    echo "4. 卸载 Docker Engine"
+    echo "5. 卸载 Docker Compose"
+    echo "6. 退出"
 
     read -p "请输入选项的编号： " choice
 
@@ -55,7 +91,13 @@ function main() {
             install_docker_compose
             ;;
         4)
-            echo "退出安装脚本。"
+            uninstall_docker_engine
+            ;;
+        5)
+            uninstall_docker_compose
+            ;;
+        6)
+            echo "退出脚本。"
             exit 0
             ;;
         *)
@@ -64,7 +106,7 @@ function main() {
             ;;
     esac
 
-    echo "安装完成！请注销并重新登录以使 Docker 用户组更改生效。"
+    echo "操作完成！请注销并重新登录以使 Docker 用户组更改生效（如果适用）。"
 }
 
 main

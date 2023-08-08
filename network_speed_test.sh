90644efc6c75aa26f6dc910afc35d1798341c4c1
starlgz <97614350+starlgz@users.noreply.github.com>
2023-08-08 11:40:26 +0800

    Update network_speed_test.sh

--- network_speed_test.sh
+++ network_speed_test.sh
@@ -1,19 +1,36 @@
 #!/bin/bash
 
+# 安装speedtest-cli（如果未安装）
+if ! command -v speedtest &>/dev/null; then
+    echo "安装speedtest-cli..."
+    sudo apt update
+    sudo apt install speedtest-cli -y
+fi
+
 # 测速函数
 function network_speed_test() {
-    echo "开始测试服务器：$1"
-    speedtest-cli --server $2
-    echo "==============================="
+    local server_info="$1"
+    local server_id=$(echo "$server_info" | grep -oP '^\[\d+\]' | tr -d '[]')
+    local server_name=$(echo "$server_info" | grep -oP '(?<=- ).*$')
+
+    echo "正在测试服务器：$server_name [$server_id]..."
+    speedtest-cli --server "$server_id"
+    echo "====================================="
 }
 
-# 选择测速点
+# 获取所有可用的测速服务器列表
+function get_speedtest_servers() {
+    speedtest-cli --list | grep -oP '(?<=\[\d+\] ).*(?= -)'
+}
+
+# 执行网络测速
 function main() {
-    network_speed_test "中国上海移动" 5862
-    network_speed_test "中国北京电信" 3633
-    network_speed_test "中国广州电信" 5083
-    network_speed_test "新加坡" 18220
-    network_speed_test "美国西部" 5117
+    local servers=$(get_speedtest_servers)
+    local IFS=$'\n'
+
+    for server_info in $servers; do
+        network_speed_test "$server_info"
+    done
 }
 
 main

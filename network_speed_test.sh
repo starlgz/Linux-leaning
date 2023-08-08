022284fa97d17ea18dec71d447a9b3fcb1c715c1
starlgz <97614350+starlgz@users.noreply.github.com>
2023-08-08 11:43:42 +0800

    Update network_speed_test.sh

--- network_speed_test.sh
+++ network_speed_test.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-# 安装指定版本的speedtest-cli（如果未安装）
+# 安装speedtest-cli（如果未安装）
 if ! command -v speedtest &>/dev/null; then
     echo "安装speedtest-cli..."
     sudo apt update
-    sudo apt install python3-pip -y
-    sudo pip3 install speedtest-cli==2.1.1
+    sudo apt install speedtest-cli -y
 fi
 
 # 测速函数
@@ -19,17 +18,21 @@ function network_speed_test() {
     echo "====================================="
 }
 
-# 获取所有可用的测速服务器列表
-function get_speedtest_servers() {
-    speedtest-cli --list | grep -oP '(?<=\[\d+\] ).*(?= -)'
-}
-
 # 执行网络测速
 function main() {
-    local servers=$(get_speedtest_servers)
-    local IFS=$'\n'
+    # 手动选择测速服务器，包含3个中国服务器和其他一些国际服务器
+    local servers=(
+        "[5862] 中国上海移动 - CMC"
+        "[3633] 中国北京电信 - China Telecom Beijing"
+        "[5083] 中国广州电信 - China Telecom Guangzhou"
+        "[18220] 新加坡 - Singtel (Singapore)"
+        "[1902] 印度班加罗尔 - Excitel (Bangalore, India)"
+        "[3630] 韩国首尔SKB - SK Broadband (Seoul, Korea)"
+        "[24218] 俄罗斯莫斯科 - Rostelecom (Moscow, Russia)"
+        "[24220] 西班牙马德里 - Telefonica (Madrid, Spain)"
+    )
 
-    for server_info in $servers; do
+    for server_info in "${servers[@]}"; do
         network_speed_test "$server_info"
     done
 }

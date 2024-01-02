7a01d28ead372ab9e9942c2f77f75e6594647c62
starlgz <97614350+starlgz@users.noreply.github.com>
2024-01-02 20:43:45 +0800

    Update yt-dlp.sh

--- yt-dlp.sh
+++ yt-dlp.sh
@@ -17,18 +17,16 @@ read -p "选择一个数字对应的视频格式和质量: " video_format_number
 # 选择音频格式和质量
 read -p "选择一个数字对应的音频格式和质量: " audio_format_number
 
-# 下载视频
-video_options="--format $video_format_number --merge-output-format mp4"
-$YT_DLP_COMMAND "$video_url" $video_options -o "%(title)s_video.%(ext)s"
-
-# 下载音频
-audio_options="--format $audio_format_number --merge-output-format mp4"
-$YT_DLP_COMMAND "$video_url" $audio_options -o "%(title)s_audio.%(ext)s"
+# 下载视频和音频
+video_options="--format $video_format_number"
+audio_options="--format $audio_format_number"
+$YT_DLP_COMMAND "$video_url" $video_options -o "%(title)s"
+$YT_DLP_COMMAND "$video_url" $audio_options -o "%(title)s"
 
 # 合并视频和音频
-ffmpeg -i "%(title)s_video.mp4" -i "%(title)s_audio.mp4" -c copy "%(title)s.mp4"
+ffmpeg -i "%(title)s" -i "%(title)s" -c copy "%(title)s.mp4"
 
 # 删除临时文件
-rm "%(title)s_video.mp4" "%(title)s_audio.mp4"
+rm "%(title)s"
 
 echo "视频下载并合并完成！"

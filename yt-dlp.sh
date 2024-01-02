9451a52cdb3327a8d5d470baa0e64a26adf953ca
starlgz <97614350+starlgz@users.noreply.github.com>
2024-01-02 20:48:59 +0800

    Update yt-dlp.sh

--- yt-dlp.sh
+++ yt-dlp.sh
@@ -14,19 +14,6 @@ echo "$formats"
 # 选择视频格式和质量
 read -p "选择一个数字对应的视频格式和质量: " video_format_number
 
-# 选择音频格式和质量
-read -p "选择一个数字对应的音频格式和质量: " audio_format_number
-
-# 下载视频和音频
-video_options="--format $video_format_number"
-audio_options="--format $audio_format_number"
-$YT_DLP_COMMAND "$video_url" $video_options -o "%(title)s"
-$YT_DLP_COMMAND "$video_url" $audio_options -o "%(title)s"
-
-# 合并视频和音频
-ffmpeg -i "%(title)s" -i "%(title)s" -c copy "%(title)s.mp4"
-
-# 删除临时文件
-rm "%(title)s"
-
-echo "视频下载并合并完成！"
+# 下载视频和音频并合并
+download_options="--format $video_format_number -o '%(title)s.%(ext)s'"
+$YT_DLP_COMMAND "$video_url" $download_options

5cb63262c73e2d2da2475491ce29f9dfa8cfc4b3
starlgz <97614350+starlgz@users.noreply.github.com>
2024-01-06 00:19:09 +0800

    Update slt.sh

--- slt.sh
+++ slt.sh
@@ -2,8 +2,6 @@
 
 # 设置yt-dlp命令
 YT_DLP_COMMAND="yt-dlp"
-# 设置ffmpeg命令
-FFMPEG_COMMAND="ffmpeg"
 
 # 提示用户输入视频链接
 echo "请输入视频链接:"
@@ -20,22 +18,9 @@ read -p "是否下载缩略图？ (y/n, 默认为y): " download_thumbnail
 download_thumbnail=${download_thumbnail:-y}
 
 if [ "$download_thumbnail" == "y" ]; then
-    # 下载缩略图
+    # 下载并直接转换为JPEG格式
     $YT_DLP_COMMAND --write-thumbnail --skip-download "$video_url"
     echo "缩略图已下载！"
-
-    # 检查缩略图格式并转换为JPEG
-    thumbnail_extension="${thumbnail_url##*.}"
-    if [ "$thumbnail_extension" == "webp" ]; then
-        thumbnail_filename="${thumbnail_url##*/}"
-        thumbnail_filename="${thumbnail_filename%.*}"
-        jpeg_filename="$thumbnail_filename.jpg"
-        
-        # 使用ffmpeg转换为JPEG
-        $FFMPEG_COMMAND -i "$thumbnail_filename.webp" "$jpeg_filename"
-        
-        echo "缩略图已转换为JPEG格式: $jpeg_filename"
-    fi
 else
     echo "不下载缩略图。"
 fi
